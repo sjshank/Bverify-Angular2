e7a691502145c31dcd82a8003f26b7c90e5c96dd
sjshank <598524@CTS.COM>
2017-03-22 19:11:25 +0530

    Initial Commit

---
+++ client/src/app/models/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    userName: string;
+    token: string;
+     constructor(userName:string, token:string){
+         this.userName = userName;
+         this.token = token;
+     }
+}
